e3a09a0228947464d35ea170486789e73580ea5f
jurbelo <juho.kirvesmaki@selko.io>
2019-11-11 23:20:19 +0200

    10

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -4,48 +4,40 @@ import java.util.*;
 
 public class Varasto {
 
-    private static Varasto instanssi;
 
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
 
-        return instanssi;
-    }
-    
     private Kirjanpito kirjanpito;
-    private HashMap<Tuote, Integer> saldot;  
-    
-    private Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
+    private HashMap<Tuote, Integer> saldot;
+
+    public Varasto(Kirjanpito k) {
+        kirjanpito = k;
         saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
-            
+
     public Tuote haeTuote(int id){
         for (Tuote t : saldot.keySet()) {
             if ( t.getId()==id) return t;
         }
-        
+
         return null;
     }
 
     public int saldo(int id){
         return saldot.get(haeTuote(id));
     }
-    
-    public void otaVarastosta(Tuote t){        
+
+    public void otaVarastosta(Tuote t){
         saldot.put(t,  saldo(t.getId())-1 );
         kirjanpito.lisaaTapahtuma("otettiin varastosta "+t);
     }
-    
+
     public void palautaVarastoon(Tuote t){
         saldot.put(t,  saldo(t.getId())+1 );
         kirjanpito.lisaaTapahtuma("palautettiin varastoon "+t);
-    }    
-    
-    private void alustaTuotteet() {
+    }
+
+    public void alustaTuotteet() {
         saldot.put(new Tuote(1, "Koff Portteri", 3), 100);
         saldot.put(new Tuote(2, "Fink Bräu I", 1), 25);
         saldot.put(new Tuote(3, "Sierra Nevada Pale Ale", 5), 30);
